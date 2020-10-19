0fc206400da80b7affa6aef9fc044065add539ca
Eric Farnan <ericfarnan94@gmail.com>
2020-10-19 16:38:35 -0500

    - Fixed clear item from cart reducer case not removing the item -  Added add and remove item functionality to the checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     // if the item did not exist already in the cart spread in the existing cart items and the new item with the quantity 1
     return [...cartItems, { ...cartItemToAdd, quantity: 1}]
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    // find the existing cart item
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    // if the quantity is 1 then filter out the item
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    // if there is more than 1 item quantity then map the existing cart items with the item to remove but subtract 1 from the quantity
+    // if the item doesnt exist somehow just return the cartItem
+    return cartItems.map(
+        cartItem => cartItem.id === cartItemToRemove.id ? 
+        { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+};
