ba55bd0d6d709025f3f15a7cf3cdf7fcae9f7309
Eric Farnan <ericfarnan94@gmail.com>
2020-10-20 08:22:45 -0500

    - Added redux-persist - Added cart items to persist local storage - Created directory redux for directory items

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,26 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// local storage
+import storage from 'redux-persist/lib/storage';
 
 import loginRegisterReducer from './login-register/login-register.reducer';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
-export default combineReducers({
+// config for persistance
+const persistConfig = {
+    key: 'root',
+    storage,
+    // items we want to persist
+    whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     loginRegister: loginRegisterReducer,
     user: userReducer,
-    cart: cartReducer  
+    cart: cartReducer,
+    directory: directoryReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
