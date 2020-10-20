154de9c1d9d98952cd9931e088143e8d997cc259
Eric Farnan <ericfarnan94@gmail.com>
2020-10-20 12:32:46 -0500

    - Converted shop.data.js into an object - Added a new selector for shop to handle shop.data.js object - Added collection page component view

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,13 +4,19 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 import './collection.styles.scss';
 
-const CollectionPage = ({match, collection}) => (
+const CollectionPage = ({match, collection}) => {
+    const {title, items } = collection;
 
+    return (
     <div className='collection-page'>
         {console.log(collection)}
-        <h2>COLLECTION PAGE</h2>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            { items.map(item => (<CollectionItem key={item.id} item={item}/>)) }
+        </div>
     </div>
-);
+    );
+};
 
 // ownProps are the existing properties on the component
 const mapStateToProps = (state, ownProps) => ({
